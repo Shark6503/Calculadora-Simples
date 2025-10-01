d0630fd5d3f5271cf3d3317da068b5f2b113935f
Shark6503 <shark06503@sharkcoders.pt>
2025-10-01 18:13:34 +0100

    Add files via upload

---
+++ medias.py
@@ -0,0 +1,7 @@
+#calcular as medias de 3 numeros introduzidos pelo utilizador
+
+a=int(input("Insira um numero"))
+b=int(input("Insira outro numero"))
+c=int(input("Insira outro numero"))
+media=(a+b+c)/3
+print (media)
