7474167aebb66bbf65c1c07da243eeaea2992952
Shark6503 <shark06503@sharkcoders.pt>
2025-10-22 18:14:24 +0100

    Add files via upload

---
+++ aprovado.py
@@ -0,0 +1,9 @@
+nota1=int(input("Insira um numero"))
+nota2=int(input("Insira outro numero"))
+
+media=(nota1+nota2)/2
+print (media)
+if media>50:
+    print("Aprovado:",media)
+else:
+    print("Reprovado:",media)
