169cfd2b2936a6044c85bb2553e644dde42fd921
Shark6503 <shark06503@sharkcoders.pt>
2025-11-19 18:16:18 +0000

    Add files via upload

---
+++ while.py
@@ -0,0 +1,18 @@
+i=0
+while i!=10:
+    i+=2
+    print(i)
+while i< 20:
+    i+=5
+    print(i)
+while True:
+    i+=1
+    print(i)
+    if i==50:
+        break
+while True:
+    i-=1
+    print(i)
+    if i==1:
+        break
+print("finished")
